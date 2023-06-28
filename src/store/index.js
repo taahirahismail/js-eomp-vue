0a67a8ee14cceb933ae0eae7d8940dda296cfeae
taahirahismail <taahirah.i@gmail.com>
2023-06-28 23:41:51 +0200

    giving up

--- src/store/index.js
+++ src/store/index.js
@@ -25,12 +25,12 @@ export default createStore({
   },
   actions: {
     getProducts: async (context) => {
-      fetch("http://localhost:3000/books")
+      fetch("http://localhost:3000/products")
       .then ((res) => res.json())
       .then ((products) => context.commit("setProducts", products));
     },
     getProduct: async (context, id) => {
-      fetch("http://localhost:3000/books/" + id)
+      fetch("http://localhost:3000/products/" + id)
       .then((res) => res.json())
       .then((product) => context.commit("setProduct", product));
     }
